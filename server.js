a7bda68c8434125c928f16f739afc323ee2be6a1
igorbojk <igorbojk@gmail.com>
2018-02-19 13:43:03 +0200

    start save

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+
+var app = express();
+
+var artists = [
+    {
+        id: 1,
+        name: 'Metallica'
+    },
+    {
+        id: 2,
+        name: 'Iron Mauden'
+    },
+    {
+        id: 3,
+        name: 'Deep Purple'
+    }
+];
+
+app.get('/', function (req, res) {
+    res.send('Hello API');
+});
+
+app.get('/artists', function (req, res) {
+    res.send(artists);
+});
+
+app.get('/artists/:id', function (req, res) {
+
+    let artist = artists.find(i => {
+        return i.id === +req.params.id;
+    });
+
+    res.send(artist);
+});
+
+app.listen(3012, function () {
+    console.log('API app started');
+});
