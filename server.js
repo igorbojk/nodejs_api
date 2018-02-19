28aedd908320191538417f463e0927c817be0fb1
igorbojk <igorbojk@gmail.com>
2018-02-19 16:30:06 +0200

    add controllers and models

--- server.js
+++ server.js
@@ -1,39 +1,36 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var artistsController = require('./artists/artists.controller');
 
 var app = express();
+var db = require('./db');
 
-var artists = [
-    {
-        id: 1,
-        name: 'Metallica'
-    },
-    {
-        id: 2,
-        name: 'Iron Mauden'
-    },
-    {
-        id: 3,
-        name: 'Deep Purple'
-    }
-];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
     res.send('Hello API');
 });
 
-app.get('/artists', function (req, res) {
-    res.send(artists);
-});
+app.get('/artists', artistsController.all);
 
-app.get('/artists/:id', function (req, res) {
+app.get('/artists/:id', artistsController.findById);
 
-    let artist = artists.find(i => {
-        return i.id === +req.params.id;
+app.post('/artists', artistsController.create);
+
+app.put('/artists/:id', artistsController.update);
+
+app.delete('/artists/:id', artistsController.delete);
+
+
+
+db.connect('mongodb://localhost:27017/myapi', function (err) {
+    if (err){
+        return console.log(err);
+    }
+    app.listen(3012, function () {
+        console.log('API app started');
     });
-
-    res.send(artist);
-});
-
-app.listen(3012, function () {
-    console.log('API app started');
 });
